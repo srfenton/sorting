429fb6432d35070a9aebed57d97359d45929d557
s fenton <sfenton@sfs-MacBook-Pro.local>
2023-02-04 13:04:47 -0800

    bubble sort is good to go :)

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+void bubble_sort(int arr[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+    for(int j = 0; j < n - i - 1; j++) {
+      if(arr[j] > arr[j+1]){
+        int tmp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = tmp; 
+      }
+    }    
+  }
+} 
+
+void print_array(int arr[], int n){
+  for(int i = 0; i < n; i++){
+    std::cout << arr[i] << std::endl;
+  }
+}
+
+
 int main() {
-  std::cout << "hello, world";
+  int driver_array[] = {1, 8, 6, 5, 4, 9, 9, 4, 5};
+  int size = sizeof(driver_array) / sizeof(driver_array[0]);
+  bubble_sort(driver_array, size);
+  print_array(driver_array, size);
   return 0;
 }
