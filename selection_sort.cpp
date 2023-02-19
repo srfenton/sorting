62fce7d8499d28db99a0fd42e34e65d2515fc018
sf <sf@sfs-MacBook-Air.local>
2023-02-18 20:33:52 -0800

    just got selection sort working. I was pretty excited to mess around with deque, but I ended up just using an array. Probably better anyway since it matches my bubble_sort program nicely

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
-#include <deque>
 
 void selection_sort(int arr[], int n) {
-  std::deque<int> sorted_array;
-  // std::deque<int>().swap(sorted_array);
-  int min = arr[0];
   for(int i = 0; i < n - 1; i++) {
-    for(int j = 0; j < n - i - 1; j++) {
-      if(arr[j]< arr[i]){
-        min = arr[j];
-      }
-    sorted_array.push_front(min);
-    }    
-  }
-  for(int n : sorted_array)
-        std::cout << n << ' ';
+    int min = i;
+    for(int j = i+1; j < n ; j++) {
+      if(arr[j]< arr[min]) {
+        min = j;
+      } 
+    }   
+    if(min != i) {
+      int tmp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = tmp; 
+    }
   } 
+}
 
 void print_array(int arr[], int n){
   for(int i = 0; i < n; i++){
@@ -28,6 +27,6 @@ int main() {
   int driver_array[] = {1, 8, 6, 5, 4, 9, 9, 4, 5};
   int size = sizeof(driver_array) / sizeof(driver_array[0]);
   selection_sort(driver_array, size);
-  // print_array(driver_array, size);
+  print_array(driver_array, size);
   return 0;
 }
