25dd0e3e1ccd8f579475070b7ba25dbafc249772
s fenton <sfenton@sfs-MacBook-Pro.local>
2023-04-14 15:21:46 -0700

    added quick sort.

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-void merge(int arr[], int l, int m, int r, int n){
+void merge(int arr[], int l, int m, int r, int size){
     int i = l;
     int j = m + 1;
     int k = l;
-    int temp[n];
+    int temp[size];
     while(i <= m && j <= r){
         if(arr[i] <= arr[j]){
             temp[k] = arr[i];    //left subarray value is smaller
@@ -33,12 +33,12 @@ void merge(int arr[], int l, int m, int r, int n){
     }
 }
 
-void merge_sort(int arr[], int l, int r, int n) {
+void merge_sort(int arr[], int l, int r, int size) {
   if(l < r) {
     int m = (l +r ) / 2;
-    merge_sort(arr, l, m, n);
-    merge_sort(arr, m + 1, r, n);
-    merge(arr, l, m, r, n);
+    merge_sort(arr, l, m, size);
+    merge_sort(arr, m + 1, r, size);
+    merge(arr, l, m, r, size);
   }
 }
 void print_array(int arr[], int n){
@@ -50,8 +50,8 @@ void print_array(int arr[], int n){
 
 int main() {
   int driver_array[] = {1, 8, 6, 5, 4, 9, 9, 4, 5};
-  int n = sizeof(driver_array) / sizeof(driver_array[0]);
-  merge_sort(driver_array, 0, (n - 1), n);    //starting index is 0
-  print_array(driver_array, n);
+  int size = sizeof(driver_array) / sizeof(driver_array[0]);
+  merge_sort(driver_array, 0, (size - 1), size);    //starting index is 0
+  print_array(driver_array, size);
   return 0;
 }
---
+++ quick_sort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int partition(int arr[], int s, int e){
+    int pivot = arr[e];
+    int pindex = s;
+    for(int i = s; i < e; ++i){
+        if(arr[i] < pivot){
+            int tmp = arr[i];
+            arr[i] = arr[pindex];
+            arr[pindex] = tmp; 
+            ++pindex;
+            }
+    }
+    int tmp = arr[e];
+    arr[e] = arr[pindex];
+    arr[pindex] = tmp;
+    return pindex;
+}
+
+void quick_sort(int arr[], int s, int e) {
+if(s < e){
+    int p = partition(arr, s, e);
+    quick_sort(arr, s, (p - 1));
+    quick_sort(arr, (p + 1), e);
+    }
+}
+
+void print_array(int arr[], int n){
+  for(int i = 0; i < n; ++i){
+    std::cout << arr[i] << std::endl;
+  }
+}  
+
+int main() {
+  int driver_array[] = {1, 8, 6, 5, 4, 9, 9, 4, 5};
+  int size = sizeof(driver_array) / sizeof(driver_array[0]);
+  quick_sort(driver_array, 0, (size - 1));
+  print_array(driver_array, size);
+  return 0;
+}
