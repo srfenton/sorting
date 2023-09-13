ff6384746c10ff72866a7441bb08e9c449133796
s fenton <sfenton@sfs-MacBook-Pro.local>
2023-09-13 10:52:42 -0700

    just updating the .gitignore

--- counting_sort.cpp
+++ counting_sort.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <algorithm>
+
 void counting_sort(int arr[], int n) {
   int output_arr[n];
-  int& r = *std::max_element(arr, arr+n);       //calculate range value
+  int& r = *std::max_element(arr, arr+n);         //calculate range value
   
-  int count_arr[r];                             //initialize array with the size of the max element of the input array and 0 values
+  int count_arr[r];                               //initialize array with the size of the max element of the input array and 0 values
   for (int i = 0; i < r; ++i) {
         count_arr[i] = 0;
     }
@@ -13,14 +14,14 @@ void counting_sort(int arr[], int n) {
     ++count_arr[arr[i]];
   }
   
-  for(int i = 1; i < r+1; ++i){                 //cumulative addition to determine the index number for each value in the array
+  for(int i = 1; i < r+1; ++i){                   //cumulative addition to determine the index number for each value in the array
     count_arr[i] = count_arr[i] + count_arr [i - 1] ;
   }
 
-  for(int i = 0; i < n; ++i){                 //map each value in the input array to its position in the output array
-    output_arr[--count_arr[arr[i]]] = arr[i]; //decrementing the value in the count_arr each time to account for repeat values
+  for(int i = 0; i < n; ++i){                     //map each value in the input array to its position in the output array
+    output_arr[--count_arr[arr[i]]] = arr[i];     //decrementing the value in the count_arr each time to account for repeat values
   }
-  for(int i = 0; i < n; ++i){                 //transfer output array values to the input array
+  for(int i = 0; i < n; ++i){                     //transfer output array values to the input array
     arr[i] = output_arr[i];
   }
 
--- radix_sort.cpp
+++ radix_sort.cpp
@@ -1,21 +1,45 @@
 #include <iostream>
+#include <algorithm>
 
-void counting_sort(it arr[], )
+void counting_sort(int arr[], int n) {
+  int output_arr[n];
+  int& r = *std::max_element(arr, arr+n);         //calculate range value
+  
+  int count_arr[r];                               //initialize array with the size of the max element of the input array and 0 values
+  for (int i = 0; i < r; ++i) {
+        count_arr[i] = 0;
+    }
 
-void radix_sort(int arr[], ) {
+  for(int i = 0; i < n; ++i){
+    ++count_arr[arr[i]];
+  }
+  
+  for(int i = 1; i < r+1; ++i){                   //cumulative addition to determine the index number for each value in the array
+    count_arr[i] = count_arr[i] + count_arr [i - 1] ;
+  }
 
+  for(int i = 0; i < n; ++i){                     //map each value in the input array to its position in the output array
+    output_arr[--count_arr[arr[i]]] = arr[i];     //decrementing the value in the count_arr each time to account for repeat values
+  }
+  for(int i = 0; i < n; ++i){                     //transfer output array values to the input array
+    arr[i] = output_arr[i];
+  }
 }
+
+// void radix_sort(int arr[], int n) {
+
+// }
 
 void print_array(int arr[], int n){
   for(int i = 0; i < n; ++i){
     std::cout << arr[i] << std::endl;
   }
-}  
+}
 
 int main() {
   int driver_array[] = {1, 8, 6, 5, 4, 9, 9, 4, 5};
   int size = sizeof(driver_array) / sizeof(driver_array[0]);
-  radix_sort(driver_array, size);
+  // radix_sort(driver_array, size);
   print_array(driver_array, size);
   return 0;
 }
